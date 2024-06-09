f006ff9c78f8d93f328dea6ba227374325b9e2ac
yocjyet <git@yocjyet.dev>
2024-06-09 16:15:49 +0900

    Remove About in Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,29 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import paraglide from "@inlang/paraglide-astro";
 import cloudflare from "@astrojs/cloudflare";
+import icon from "astro-icon";
+// import Icons from "unplugin-icons";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  i18n: {
+    locales: ["en", "ja", "zh", "fr"],
+    defaultLocale: "en",
+  },
+  integrations: [
+    tailwind(),
+    paraglide({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
+    icon(),
+    svelte(),
+  ],
   output: "server",
   adapter: cloudflare(),
+  // vite: {
+  //   plugins: [Icons({ compiler: "svelte" })],
+  // },
 });
