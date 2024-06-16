5f47b26dd800b5f6b9c86394be8b8a8ce5c39d0d
yocjyet <git@yocjyet.dev>
2024-06-17 01:22:40 +0900

    Add light-dark theme switcher

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      // 		--accent: 136, 58, 234;
+      // --accent-light: 224, 204, 250;
+      // --accent-dark: 49, 10, 101;
+      colors: {
+        accent: {
+          DEFAULT: "rgb(136, 58, 234)",
+          light: "rgb(224, 204, 250)",
+          dark: "rgb(49, 10, 101)",
+        },
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "selector",
+};
