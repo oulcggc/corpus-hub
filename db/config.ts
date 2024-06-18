2e2fc93425438ca88178b21af6539bdd1d9ade12
于拙 <git@yocjyet.dev>
2024-06-18 13:28:37 +0000

    Add more tables

--- db/config.ts
+++ db/config.ts
@@ -13,7 +13,49 @@ const Authors = defineTable({
   },
 });
 
+const Concordancers = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    lang: column.text(),
+    nameJa: column.text(),
+    nameEn: column.text(),
+    authorId: column.text(),
+    link: column.text(),
+    usageOnline: column.boolean(),
+    usageFree: column.boolean(),
+    usageFreemium: column.boolean(),
+    usageRegistration: column.boolean(),
+    usageApplication: column.boolean(),
+  },
+});
+
+const Corpora = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    lang: column.text(),
+    authorId: column.text(),
+    nameJa: column.text(),
+    nameEn: column.text(),
+    abbr: column.text(),
+    link: column.text(),
+  },
+});
+
+const ConcordancerCorpora = defineTable({
+  columns: {
+    concordancerId: column.text(),
+    corporaId: column.text(),
+  },
+});
+
+const CorporaTags = defineTable({
+  columns: {
+    corporaId: column.text(),
+    tag: column.text(),
+  },
+});
+
 // https://astro.build/db/config
 export default defineDb({
-  tables: { Authors },
+  tables: { Authors, Concordancers, Corpora, ConcordancerCorpora, CorporaTags },
 });
